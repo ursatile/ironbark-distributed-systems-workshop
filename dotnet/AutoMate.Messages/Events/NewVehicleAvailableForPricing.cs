442ae46ab3b39510b14581e33b9e97442b0e0be3
Dylan Beattie <dylan@dylanbeattie.net>
2022-07-19 08:25:55 +0100

    Hayden's StatusChecker implementation

--- dotnet/AutoMate.Messages/Events/NewVehicleAvailableForPricing.cs
+++ dotnet/AutoMate.Messages/Events/NewVehicleAvailableForPricing.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
 namespace AutoMate.Messages.Events {
-    public class NewVehicleListed {
+    public class NewVehicleAvailableForPricing {
         public string Manufacturer { get; set; }
         public string VehicleModel { get; set; }
         public string Registration { get; set; }
         public string Color { get; set; }
         public int Year { get; set; }
-        public DateTimeOffset ListedAt { get; set; }
+        public DateTimeOffset AvailabilityConfirmedAt { get; set; }
     }
 }
--- dotnet/AutoMate.StatusChecker/Program.cs
+++ dotnet/AutoMate.StatusChecker/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net.Http;
 using System.Threading.Tasks;
-using AutoMate.Messages.Events;
 using MassTransit;
 using Microsoft.Extensions.Hosting;
 
@@ -15,7 +13,7 @@ namespace AutoMate.StatusChecker {
                     services.AddMassTransit(mt => {
                         mt.UsingRabbitMq((context, config) => {
                             config.Host(RABBITMQ_URL);
-                            config.ReceiveEndpoint("dylanbeattie-automate-vehicle-status-checker", e => {
+                            config.ReceiveEndpoint("dylanbeattie", e => {
                                 e.Consumer(() => new NewVehicleStatusChecker());
                             });
                         });
@@ -23,37 +21,6 @@ namespace AutoMate.StatusChecker {
                 })
                 .Build().RunAsync();
             Console.WriteLine("AutoMate.AuditLog running! Press Ctrl-C to quit.");
-        }
-    }
-    public class NewVehicleStatusChecker : IConsumer<NewVehicleListed> {
-        private static readonly HttpClient httpClient = new HttpClient();
-        public async Task Consume(ConsumeContext<NewVehicleListed> context) {
-            var message = context.Message;
-            var csv = $"{message.Registration},{message.Manufacturer},{message.VehicleModel},{message.Color},{message.Year},{message.ListedAt:O}";
-            await Console.Out.WriteLineAsync(csv);
-            var url =
-                $"https://ursatile-vehicle-info-checker.azurewebsites.net/api/CheckVehicleStatus?registration={message.Registration}";
-            var status = await httpClient.GetStringAsync(url);
-            await Console.Out.WriteLineAsync($"Vehicle status: {status}");
-            switch (status) {
-                case "OK":
-                    var vehicleReadyForPricing = new VehicleReadyForPricing {
-                        VehicleModel = context.Message.VehicleModel,
-                        Color = context.Message.Color,
-                        Registration = context.Message.Registration,
-                        Year = context.Message.Year
-                    };
-                    await context.Publish(vehicleReadyForPricing);
-                    await Console.Out.WriteLineAsync("Published VehicleReadyForPricing!");
-                    break;
-                case "WRITTEN_OFF":
-                case "STOLEN":
-                    // TODO: what happens here?
-                    break;
-                default:
-                    await Console.Out.WriteLineAsync("Invalid vehicle status. Oops.");
-                    break;
-            }
         }
     }
 }
