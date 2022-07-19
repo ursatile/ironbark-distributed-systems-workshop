db62b95b0a5a3c3144eea5889b520b6cdafe84f7
Dylan Beattie <dylan@dylanbeattie.net>
2022-07-19 09:55:55 +0100

    Added AutoMate.PricingClient in gRPC

--- dotnet/AutoMate.PricingClient/Program.cs
+++ dotnet/AutoMate.PricingClient/Program.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutoMate.Messages.Events;
+using Grpc.Net.Client;
 using MassTransit;
 using Microsoft.Extensions.Hosting;
+using Autobarn.PricingEngine;
 
 namespace AutoMate.PricingClient {
     class Program {
         private const string RABBITMQ_URL =
             "amqps://karekqvh:5NidEd8zPSU1DIdg-kFcMB0D3B3Ws9nY@hefty-silver-gopher.rmq4.cloudamqp.com/karekqvh";
+
+        private const string GRPC_URL = "https://workshop.ursatile.com:5003/";
+
         static async Task Main(string[] args) {
+            using var channel = GrpcChannel.ForAddress(GRPC_URL);
+            var grpcClient = new Pricer.PricerClient(channel);
             await Host.CreateDefaultBuilder(args)
                 .ConfigureServices(services => {
                     services.AddMassTransit(mt => {
                         mt.UsingRabbitMq((context, config) => {
                             config.Host(RABBITMQ_URL);
                             config.ReceiveEndpoint("automate-pricing-client", e => {
-                                e.Consumer(() => new NewVehiclePriceCalculator());
+                                e.Consumer(() => new NewVehiclePriceCalculator(grpcClient));
                             });
                         });
                     });
@@ -26,8 +33,23 @@ namespace AutoMate.PricingClient {
     }
 
     public class NewVehiclePriceCalculator : IConsumer<NewVehicleAvailableForPricing> {
+        private readonly Pricer.PricerClient grpcClient;
+
+        public NewVehiclePriceCalculator(Pricer.PricerClient grpcClient) {
+            this.grpcClient = grpcClient;
+        }
         public async Task Consume(ConsumeContext<NewVehicleAvailableForPricing> context) {
-            await Console.Out.WriteLineAsync(context.Message.Registration);
+            await Console.Out.WriteLineAsync(
+                $"Using gRPC to get price for {context.Message.Manufacturer} {context.Message.VehicleModel}...");
+            var request = new PriceRequest {
+                Color = context.Message.Color,
+                Year = context.Message.Year,
+                Make = context.Message.Manufacturer,
+                Model = context.Message.VehicleModel
+            };
+            var reply = await grpcClient.GetPriceAsync(request);
+            await Console.Out.WriteLineAsync($"Got a price! {reply.Price} {reply.CurrencyCode}");
+            // TODO: publish something here?
         }
     }
 }
