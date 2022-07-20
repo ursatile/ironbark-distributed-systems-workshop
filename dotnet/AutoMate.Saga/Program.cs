3a9a4478cfcd28668d2c98faa57e27bb375618f0
Dylan Beattie <dylan@dylanbeattie.net>
2022-07-20 07:56:12 +0100

    About to do a dangerous rename...

---
+++ dotnet/AutoMate.Saga/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+
+namespace AutoMate.Saga {
+    class Program {
+        private const string RABBITMQ_URL =
+            "amqps://karekqvh:5NidEd8zPSU1DIdg-kFcMB0D3B3Ws9nY@hefty-silver-gopher.rmq4.cloudamqp.com/karekqvh";
+        static async Task Main(string[] args) {
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => {
+                    services.AddMassTransit(mt => {
+                        mt.UsingRabbitMq((context, config) => {
+                            config.Host(RABBITMQ_URL);
+                            config.ConfigureEndpoints(context);
+                        });
+                        mt.SetKebabCaseEndpointNameFormatter();
+                    });
+                })
+                .Build().RunAsync();
+            Console.WriteLine("AutoMate.AuditLog running! Press Ctrl-C to quit.");
+        }
+    }
+
+    public class VehicleListingSaga : MassTransitStateMachine<VehicleListingState> {
+
+        public State Submitted { get; private set; }
+        public State AwaitingPrice { get; private set; }
+        public State Stolen { get; private set; }
+        public State WrittenOff { get; private set; }
+    }
+}
---
+++ dotnet/AutoMate.Saga/VehicleListingState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MassTransit;
+
+namespace AutoMate.Saga {
+    public class VehicleListingState : SagaStateMachineInstance {
+        public Guid CorrelationId { get; set; }
+        public string Registration { get; set; }
+        public State CurrentState { get; set; }
+    }
+}
